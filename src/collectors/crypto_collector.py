27fd98cbf6279c3e1a19d9ad5ad9806ccbf3fd91
mshimizuDev <mshimizuDev2008@gmail.com>
2026-03-18 20:15:58 +0900

    refactor: project structure for AWS and add config

--- src/collectors/crypto_collector.py
+++ src/collectors/crypto_collector.py
@@ -1,17 +1,32 @@
 import requests
 import datetime
 
-url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
+from src.utils.csv_writer import save_to_csv
+from src.utils.config import CRYPTO_CSV
 
-response = requests.get(url)
-data = response.json()
+def fetch_crypto_price():
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
 
-btc_price = data["bitcoin"]["usd"]
-eth_price = data["ethereum"]["usd"]
+    response = requests.get(url)
+    data = response.json()
 
-now = datetime.datetime.now()
+    btc_price = data["bitcoin"]["usd"]
+    eth_price = data["ethereum"]["usd"]
 
-with open("../data/crypto_price.csv", "a") as f:
-    f.write(f"{now},{btc_price},{eth_price}\n")
+    return btc_price, eth_price
 
-print("BTC:", btc_price, "ETH:", eth_price)
+
+def main():
+    btc_price, eth_price = fetch_crypto_price()
+
+    now = datetime.datetime.now()
+
+    row = [now, btc_price, eth_price]
+
+    save_to_csv(CRYPTO_CSV, row)
+
+    print("BTC:", btc_price, "ETH:", eth_price)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils/config.py
@@ -0,0 +1,3 @@
+DATA_DIR = "data"
+
+CRYPTO_CSV = f"{DATA_DIR}/crypto_price.csv"
